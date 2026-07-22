300400f22c6b3c89b22c495e725e4ede6d693058
cardamom7 <ayaka.yamaki.gene@gmail.com>
2026-07-23 02:38:00 +0900

    Update data_dougu.js

--- data_dougu.js
+++ data_dougu.js
@@ -1,1 +1,198 @@
-
+window.CRAFT_DATA["道具鍛冶職人"] = {
+  "ブーメラン": [
+    {
+      name: "輝天のブーメラン",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "デモニウム鉱石", count: 15, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "パルミラヴーメラン",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "デモニウム鉱石", count: 12, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "魔導士のブーメラン",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 25, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ゴールドストーン", count: 15, shop: "バザー", defaultPrice: 2000 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    },
+    {
+      name: "プロミネンス",
+      level: "装備Lv 100",
+      materials: [
+        { name: "プラチナ鉱石", count: 22, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ゴールドストーン", count: 12, shop: "バザー", defaultPrice: 2000 },
+        { name: "ようせいのひだね", count: 10, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "ツメ": [
+    {
+      name: "輝天のツメ",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "ドラゴン骨", count: 15, shop: "バザー", defaultPrice: 5000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "ゴッドクロー",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 28, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ドラゴン骨", count: 12, shop: "バザー", defaultPrice: 5000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "悪鬼のツメ",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 24, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "大きな骨", count: 20, shop: "バザー", defaultPrice: 1500 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    },
+    {
+      name: "呪眼のツメ",
+      level: "装備Lv 100",
+      materials: [
+        { name: "プラチナ鉱石", count: 20, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "大きな骨", count: 16, shop: "バザー", defaultPrice: 1500 },
+        { name: "ようせいのひだね", count: 10, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "ハンマー": [
+    {
+      name: "輝天のハンマー",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "デモニウム鉱石", count: 15, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "メガトンハンマー",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ヘビーメタル", count: 15, shop: "バザー", defaultPrice: 6000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "魔犬のツインハンマー",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 25, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ヘビーメタル", count: 12, shop: "バザー", defaultPrice: 6000 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "ヤリ": [
+    {
+      name: "輝天の槍",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "デモニウム鉱石", count: 15, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "オウマガキ",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ドラゴン骨", count: 15, shop: "バザー", defaultPrice: 5000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "轟雷のやり",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 26, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "雷鳴の玉", count: 12, shop: "バザー", defaultPrice: 5000 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "職人道具": [
+    {
+      name: "超鍛冶ハンマー",
+      level: "職人Lv 40",
+      materials: [
+        { name: "プラチナ鉱石", count: 15, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ゴールドストーン", count: 8, shop: "バザー", defaultPrice: 2000 },
+        { name: "超ようせいのひだね", count: 2, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "光の鍛冶ハンマー",
+      level: "職人Lv 45",
+      materials: [
+        { name: "光のたね", count: 15, shop: "バザー", defaultPrice: 4000 },
+        { name: "超ようせいのひだね", count: 3, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "超フライパン",
+      level: "職人Lv 40",
+      materials: [
+        { name: "プラチナ鉱石", count: 15, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "大きな貝ガラ", count: 10, shop: "両方", place: "ジュレット素材屋", defaultPrice: 240 },
+        { name: "超ようせいのひだね", count: 2, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "光のフライパン",
+      level: "職人Lv 45",
+      materials: [
+        { name: "光のたね", count: 15, shop: "バザー", defaultPrice: 4000 },
+        { name: "超ようせいのひだね", count: 3, shop: "バザー", defaultPrice: 32000 }
+      ]
+    }
+  ],
+  "家具・庭具": [
+    {
+      name: "プラチナのイス",
+      level: "職人Lv 20",
+      materials: [
+        { name: "プラチナ鉱石", count: 5, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "きたないきぬのふく", count: 2, shop: "バザー", defaultPrice: 100 }
+      ]
+    },
+    {
+      name: "金のタンス",
+      level: "職人Lv 30",
+      materials: [
+        { name: "ゴールドストーン", count: 10, shop: "バザー", defaultPrice: 2000 },
+        { name: "プラチナ鉱石", count: 10, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 }
+      ]
+    },
+    {
+      name: "プラチナの大きな置物",
+      level: "職人Lv 35",
+      materials: [
+        { name: "プラチナ鉱石", count: 20, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ウルベア金貨", count: 2, shop: "バザー", defaultPrice: 2000 }
+      ]
+    }
+  ]
+};
